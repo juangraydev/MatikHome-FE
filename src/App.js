27b0892455e86e77f5779b54dae58884e2db3348
juangraydev <jwsabala@gmail.com>
2023-07-25 10:50:30 +0800

    remove google

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-
-import { useEffect } from "react";
 import Router from "./router/index"
-import jwtDecode from "jwt-decode";
-import { useDispatch } from 'react-redux'
-import { successUserData } from "./modules/auth/store/actionCreators"
-
 function App() {
-	const dispatch = useDispatch()
-  
-  const handleCallbackREsponse = (response) => {
-    dispatch(successUserData(response.credential));
-  }
-
-  useEffect(()=>{
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: process.env.REACT_APP_CLIENT_ID,
-      callback: handleCallbackREsponse
-    })
-    google.accounts.id.prompt();
-  }, [])
   return (
-    <div className="App">
-      <Router/>
-    </div>
+    <Router/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react'
@@ -38,20 +37,18 @@ const theme = createTheme({
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Provider store={ store }>
-          <PersistGate persistor={ persistor }>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </PersistGate>
-        </Provider> 
-      </ThemeProvider>
-    </React.StrictMode>,
-  // </GoogleOAuthProvider>,
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Provider store={ store }>
+        <PersistGate persistor={ persistor }>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider> 
+    </ThemeProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
